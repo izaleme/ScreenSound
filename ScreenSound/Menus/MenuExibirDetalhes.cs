48fde1074123b197db2869dde5cc8acb82e3dec4
Izabela Leme <izabela.leme@plasutil.com.br>
2024-06-10 11:18:36 -0300

    Organizando o cód de acordo com boas práticas

---
+++ ScreenSound/Menus/MenuExibirDetalhes.cs
@@ -0,0 +1,31 @@
+﻿using ScreenSound.Models;
+
+namespace ScreenSound.Menus;
+
+internal class MenuExibirDetalhes : Menu
+{
+    public void Executar(Dictionary<string, Banda> bandasRegistradas)
+    {
+        Console.Clear();
+        ExibirTituloDaOpcao("Exibir detalhes da banda");
+        //ExibirNomesBandas();
+        Console.Write("Digite o nome da banda que deseja conhecer melhor: ");
+
+        string nomeDaBanda = Console.ReadLine()!.ToLower();
+        if (bandasRegistradas.ContainsKey(nomeDaBanda))
+        {
+            Banda banda = bandasRegistradas[nomeDaBanda];
+            Console.WriteLine($"\nA média da banda {nomeDaBanda} é {banda.Media}.");
+            Console.WriteLine("Digite uma tecla para voltar ao menu principal");
+            Console.ReadKey();
+            Console.Clear();
+        }
+        else
+        {
+            Console.WriteLine($"\nA banda {nomeDaBanda} não foi encontrada!");
+            Console.WriteLine("Digite uma tecla para voltar ao menu principal");
+            Console.ReadKey();
+            Console.Clear();
+        }
+    }
+}

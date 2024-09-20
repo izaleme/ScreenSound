ba1dce23706194a59d0c05dc1178780318cd0600
Izabela Leme <izaleme00@gmail.com>
2024-09-20 16:22:20 -0300

    Implementação da classe genérica DAL

--- ScreenSound/Banco/ArtistaDAL.cs
+++ ScreenSound/Banco/ArtistaDAL.cs
@@ -4,38 +4,6 @@ namespace ScreenSound.Banco;
 
 internal class ArtistaDAL : DAL<Artista>
 {
-    private readonly ScreenSoundContext context;
+    public ArtistaDAL(ScreenSoundContext context) : base(context) { }
 
-    public ArtistaDAL(ScreenSoundContext context)
-    {
-        this.context = context;
-    }
-
-    public override IEnumerable<Artista> Listar()
-    {
-        return context.Artistas.ToList();
-    }
-
-    public override void Adicionar(Artista artista)
-    {
-        context.Artistas.Add(artista);
-        context.SaveChanges();
-    }
-
-    public override void Atualizar(Artista artista)
-    {
-        context.Artistas.Update(artista);
-        context.SaveChanges();
-    }
-    
-    public override void Deletar(Artista artista)
-    {
-        context.Artistas.Remove(artista);
-        context.SaveChanges();
-    }
-
-    public Artista? RecuperarPeloNome(string nome)
-    {
-        return context.Artistas.FirstOrDefault(a => a.Nome.Equals(nome));
-    }
 }
